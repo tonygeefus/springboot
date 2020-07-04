083bd41ee62aa0d3107ac3b4a5729efb390ac8a3
Tony Geefus S <tonygeefus@gmail.com>
2020-07-04 11:39:58 +0530

    Update UserRepository.java

--- Springboot/src/main/java/com/SQL/Connect/UserRepository.java
+++ Springboot/src/main/java/com/SQL/Connect/UserRepository.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.SQL.Connect;
 
 import org.springframework.data.repository.CrudRepository;
-/**
- *
- * @author BARATH.R.V
- */
+
 public interface UserRepository extends CrudRepository<User,Integer>{
     
 }
